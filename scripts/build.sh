433c338c84d27645f0b622c31c984c2e9a957b67
plastic041 <plastic041@icloud.com>
2026-08-21 12:21:37 +0900

    ligatures -> =>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,12 @@ rm ../dist/donguri16.bdf
 rm ../dist/donguri16.ttf
 
 echo "building new font files..."
-java -jar BitsNPicas.jar convertbitmap -f ttf -o ../dist/donguri16.ttf ../src/donguri16.kbitx
+java -jar BitsNPicas.jar convertbitmap -f ttf -o ../dist/donguri16-base.ttf ../src/donguri16.kbitx
 java -jar BitsNPicas.jar convertbitmap -f bdf -o ../dist/donguri16.bdf ../src/donguri16.kbitx
+
+echo "making ligatures..."
+fonttools feaLib -o ../dist/donguri16.ttf ligatures/liga.fea ../dist/donguri16-base.ttf
+
+rm ../dist/donguri16-base.ttf
 
 echo "Done!"
